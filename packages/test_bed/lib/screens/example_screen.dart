a9fd207d09f9834248e97b94c8269e73d5b77c77
alexayers <ayers.alex@gmail.com>
2024-05-18 22:52:28 -0400

    Basic screen test

--- packages/test_bed/lib/screens/example_screen.dart
+++ packages/test_bed/lib/screens/example_screen.dart
@@ -6,6 +6,7 @@ import 'package:teenytinytwodee/logger/logger.dart';
 import 'package:teenytinytwodee/primitives/color.dart';
 import 'package:teenytinytwodee/rendering/font.dart';
 import 'package:teenytinytwodee/rendering/renderer.dart';
+import 'package:teenytinytwodee/utils/math_utils.dart';
 
 class ExampleScreen implements GameScreen {
   final renderer = Renderer();
@@ -43,14 +44,38 @@ class ExampleScreen implements GameScreen {
 
   @override
   void renderLoop() {
+    renderer.renderImage(
+      image: ImageElement(src: 'assets/face.png'),
+      x: 0 + getRandomBetween(1, 500),
+      y: 0 + getRandomBetween(1, 500),
+      width: 50 + getRandomBetween(1, 500),
+      height: 50 + getRandomBetween(1, 500),
+    );
+
+    renderer.rect(
+      x: 0 + getRandomBetween(1, 500),
+      y: 0 + getRandomBetween(1, 500),
+      width: 50 + getRandomBetween(1, 500),
+      height: 50 + getRandomBetween(1, 500),
+      color: Color(
+        red: getRandomBetween(1, 255),
+        green: getRandomBetween(1, 255),
+        blue: getRandomBetween(1, 255),
+      ),
+    );
+
     renderer.print(
       msg: 'Hello World',
-      x: 50,
-      y: 50,
+      x: 50 + getRandomBetween(1, 500),
+      y: 50 + getRandomBetween(1, 500),
       font: Font(
         'Arial',
-        40,
-        Color(red: 255, green: 255, blue: 255),
+        500,
+        Color(
+          red: getRandomBetween(1, 255),
+          green: getRandomBetween(1, 255),
+          blue: getRandomBetween(1, 255),
+        ),
       ),
     );
   }
